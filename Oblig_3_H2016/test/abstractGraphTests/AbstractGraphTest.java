029da83f3ed644743c6c7052c913c643831a529d
Allan Arnesen <allan.arnesen@gmail.com>
2016-11-06 11:24:47 +0100

    Added test for abstractGraph

---
+++ Oblig_3_H2016/test/abstractGraphTests/AbstractGraphTest.java
@@ -0,0 +1,14 @@
+package abstractGraphTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AbstractGraphTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
