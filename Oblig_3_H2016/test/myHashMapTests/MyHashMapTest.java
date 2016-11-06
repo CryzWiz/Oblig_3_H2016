ad0f0975759fa42b5d9b2848406c5c958302f8fb
Allan Arnesen <allan.arnesen@gmail.com>
2016-11-06 12:23:25 +0100

    Added tests for MyHashMap

--- Oblig_3_H2016/test/myHashMapTests/MyHashMapTest.java
+++ Oblig_3_H2016/test/myHashMapTests/MyHashMapTest.java
@@ -1,5 +1,9 @@
 package myHashMapTests;
 import static org.junit.Assert.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import myHashMap.MyHashMap;
 import myHashMap.MyMap;
 
@@ -7,52 +11,67 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class MyHashMapTest {
+	
+	
 	public MyMap<String, Integer> map;
+	public String[] OrgKeys = {"Smith", "Anderson", "Anderson", "Lewis", "Cook", "Smith"};
+	public int[] OrgValues = {30,31,32,29,29,65};
+	public String[] KeysMinusSmith = {"Anderson", "Anderson", "Lewis", "Cook", "Smith"};
+	public int[] FalseValues = {1,3,66,94,106,28,19};
+	Set<Integer> AndersonOrgValues;
+	
 	
 	@Before
 	public void initialize(){
 		map = new MyHashMap<>();
-	    map.put("Smith", 30);
-	    map.put("Anderson", 31);
-	    map.put("Anderson", 32);
-	    map.put("Lewis", 29);
-	    map.put("Cook", 29);
-	    map.put("Smith", 65);
+		for(int i = 0; i < OrgKeys.length; i++){
+			map.put(OrgKeys[i], OrgValues[i]);
+		}
 	}
 	
 	@Test
 	public void getTheFullMapDeleteOneAndCheckIfMapIsAccurateAfterwards(){
-		System.out.println("Entries in map: " + map);
+		assertTrue(map.size() == 6);
 		map.remove("Smith");
-	    System.out.println("Entries in map: " + map);
+		for(int i = 0; i < KeysMinusSmith.length; i++){
+			assertTrue(map.containsKey(KeysMinusSmith[i]));
+		}
+		assertTrue(map.size() == 5);	
 	}
 	
 	@Test
 	public void checkIfWeCanLocateAllTheKeys(){
-		System.out.println("Entries in map: " + map);
+		for(int i = 0; i < OrgKeys.length; i++){
+			assertTrue(map.containsKey(OrgKeys[i]));
+		}
 	}
 	
 	@Test
 	public void checkIfWeCanFindAllTheValues() {
-		System.out.println("Is age 33 in the map? " + 
-			      map.containsValue(33));
+		for(int i = 0; i < OrgValues.length; i++){
+			assertTrue(map.containsValue(OrgValues[i]));
+		}
 	}
 	
 	@Test
 	public void checkIfFalseValuesGivesCorrectReturn(){
-		System.out.println("Is Arnesen in the map? " + 
-			      map.containsKey("Arnesen"));
+		for(int i = 0; i < FalseValues.length; i++){
+			assertFalse(map.containsValue(FalseValues[i]));
+		}
 	}
 	
 	@Test
 	public void checkIfWeGetAllTheValuesBehindAKey(){
-		System.out.println("All values for Key Anderson: " + map.getAll("Anderson"));
+		Set<Integer> AndersonRealValues = new HashSet<Integer>();
+		AndersonRealValues.add(31);AndersonRealValues.add(32);
+		assertEquals(map.getAll("Anderson"), AndersonRealValues);
 	}
 	
 	@Test
 	public void clearTheMapAndCheckIfItIsEmpty(){
 		map.clear();
-	    System.out.println("Entries in map: " + map);
+		String m = map.toString();
+		assertEquals(m, "[]");
 	}
 
 }
